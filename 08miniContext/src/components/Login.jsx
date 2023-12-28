dd473b6ed0edd2a8e330811af63c7b672b24a9b0
bhupesh <72373916+bhaibhupesh10@users.noreply.github.com>
2023-12-28 19:19:36 +0530

    theme switcher project using useContext api and tailwind css with mulitple components

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -1,25 +1,46 @@
-import React, {useState, useContext} from 'react'
-import UserContext from '../context/UserContext'
+import React, { useState, useContext } from 'react';
+import UserContext from '../context/userContext';
 
+// Login component
 function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-    const {setUser} = useContext(UserContext);
+  const { setUser } = useContext(UserContext);
 
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        setUser({username, password});
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
+  };
+
   return (
     <div>
-        <h2>Login</h2>
-        <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)} placeholder='username'></input>
-       {" "} <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='password'></input>
-        <button onClick={handleSubmit}>Submit</button>
+      <h2>Login</h2>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      {' '}
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <input
+          type={showPassword ? 'text' : 'password'}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <i
+          style={{ marginLeft: '5px', cursor: 'pointer' }}
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? '👁️' : '👁️‍🗨️'}
+        </i>
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
-
-  )
+  );
 }
 
 export default Login;
