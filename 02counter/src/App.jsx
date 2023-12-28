66af4853c73b40beba757dd118cce28760a07f68
bhupesh <72373916+bhaibhupesh10@users.noreply.github.com>
2023-12-28 16:00:10 +0530

    updated code

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import {useReducer} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -26,7 +24,7 @@ function App() {
       <button onClick={addValue}>Add value</button><br/>
       <button onClick={remValue}>remove value</button>
     </>
-  )
+    )
   }
 
 
